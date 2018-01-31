06def87c5cf56ca24ee867e0e621513d0a6006c8
rodriguesyoann <rodriguesyoann@MacBook-Pro-de-Yoann.local>
2018-01-31 14:54:49 +0100

    correction integration

--- result.php
+++ result.php
@@ -3,6 +3,9 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 
+
+<body class="background">
+
 <?php
 
 require_once('functions.php');
@@ -16,44 +19,54 @@ if (!empty($_POST)) {
 			
 			$tri = $_POST['tri'];
 			$start = (float) array_sum(explode(' ',microtime()));
-			echo 'Nombre itérations :'. $tri($list) . '<br />';
+			$count=$tri($list);
 			$end = (float) array_sum(explode(' ',microtime()));
 			$time = $end-$start;
+?>
+				<div class="title">
+					<h2>Résultats</h2>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="results">
+						<?php
+						foreach ($list as $element) {
+							echo $element.'</br>';
+						}
+						?>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="data">
+							<?php echo $_POST['tri'] ?>
+							<h4>Temps de traitement</h4>
+							<?php echo sprintf("%.8f", $time)." seconds."; ?>
+							<h4>Nombres d'itérations</h4>
+							<?php echo 'Nombre itérations : '.$count; ?></br></br>
+							<a href=".">Retour aux tris</a>
+						</div>
+					</div>
+				</div>
+				</div>
+<?php
 	
 		} else {
-			echo "Il n'y a pas que des nombres dans ta liste !";
-			echo '<a href=".">Retour</a>';
+			?>
+		<div class="error">
+			<h2>Il n'y a pas que des nombres dans ta liste !</h2>
+			<a href=".">Retour</a>
+		</div>
+		<?php
 		}
 
 	} else {
-		echo "Formulaire incomplet !";
-		echo '<a href=".">Retour</a>';
+		?>
+	<div class="error">
+		<h2>Formulaire incomplet !</h2>
+		<a href=".">Retour</a>
+	</div>
+	<?php
 	}
 }
 ?>
-
-<body class="background">
-	<div class="title">
-		<h2>Résultats</h2>
-	</div>
-	<div class="row">
-		<div class="col-md-6">
-			<div class="results">
-			<?php
-			foreach ($list as $element) {
-				echo $element.'</br>';
-			}
-			?>
-			</div>
-		</div>
-		<div class="col-md-6">
-			<div class="data">
-				<h4>Temps de traitement</h4>
-				<?php echo sprintf("%.8f", $time)." seconds."; ?>
-				<h4>Nombres d'itérations</h4>
-				<?php ; ?>
-			</div>
-		</div>
-	</div>
-	</div>
 </body>
